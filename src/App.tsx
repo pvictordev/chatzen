074ea2a592a271c544483b156250bf37bb8fae6f
Purice Victor <pvictor0110@gmail.com>
2023-10-16 09:39:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import Logo from "./img/chatzen-icon.png"
 import "./App.css";
 
 function App() {
-  return <>Chatzen</>;
+  return <><img src={Logo} alt="" />Chatzen</>;
 }
 
 export default App;
