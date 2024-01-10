996284635ff3a54e28c0eabcd5726f4eee194ce9
pvictordev <pvictor0110@gmail.com>
2024-01-10 20:14:42 +0200

    fixed bugs

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-appColor text-primary-foreground hover:bg-primary/90",
+        default: "bg-appColor text-primary-foreground hover:bg-purple-700",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
