98d4ba988835586984c83b2358b6181b9e9a8232
pvictordev <pvictor0110@gmail.com>
2024-01-14 17:04:48 +0200

    file type

--- src/components/Dialogue.tsx
+++ src/components/Dialogue.tsx
@@ -1,9 +1,10 @@
 import { IoImagesOutline } from "react-icons/io5";
 import { BsEmojiLaughing, BsReply } from "react-icons/bs";
 import { FiSend } from "react-icons/fi";
-import { IoIosInformationCircle } from "react-icons/io";
+import { IoIosInformationCircle, IoMdDownload } from "react-icons/io";
 import { ImFilesEmpty } from "react-icons/im";
-import { FaRegTrashAlt } from "react-icons/fa";
+import { FaRegTrashAlt, FaRegFilePdf } from "react-icons/fa";
+// FaRegFileWord;
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 import {
   Dialog,
@@ -128,6 +129,37 @@ export default function Dialogue({
           </div>
 
           <div
+            className="message-left flex flex-row-reverse my-10 items-center justify-end relative round"
+            onMouseEnter={() => setShowMessageActionsLeft(true)}
+            onMouseLeave={() => setShowMessageActionsLeft(false)}
+          >
+            <div className="message-left__block flex items-center gap-x-3">
+              <div className="message-left__text flex items-center p-3 gap-x-3 max-w-xl ml-8 relative rounded-lg shadow-lg bg-gray-300">
+                <FaRegFilePdf className="w-8 h-8" />
+                <div className="flex flex-col justify-center items-start">
+                  <span className="text-black">File</span>
+                  <span className="text-black">840.9 KB</span>
+                </div>
+                <IoMdDownload className="w-8 h-8" />
+              </div>
+              <div
+                className={
+                  showMessageActionsLeft
+                    ? "message-left-actions gap-x-3 text-slate-600 flex"
+                    : "hidden"
+                }
+              >
+                <div>
+                  <BsEmojiLaughing className="cursor-pointer" />
+                </div>
+                <div>
+                  <BsReply className="cursor-pointer" />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
             className="message-right flex flex-row-reverse my-10 items-center justify-start relative round"
             onMouseEnter={() => setShowMessageActionsRight(true)}
             onMouseLeave={() => setShowMessageActionsRight(false)}
@@ -195,15 +227,26 @@ export default function Dialogue({
         <div className="chat-message__container flex items-center gap-x-3 p-5">
           <div className="message-functionality flex gap-x-3 ">
             <div className="message-functionality__emoji">
-              <BsEmojiLaughing className="w-6 h-6 text-slate-500" />
+              <BsEmojiLaughing className="w-6 h-6 text-slate-500 cursor-pointer" />
             </div>
-            <div className="message-functionality__attach">
-              <IoImagesOutline className="w-6 h-6 text-slate-500" />
+            <div className="message-functionality__attach relative">
+              <IoImagesOutline className="w-6 h-6 text-slate-500 cursor-pointer" />
+              <input
+                type="file"
+                className="w-6 h-6 text-slate-500 absolute top-0 left-0 opacity-0 cursor-pointer"
+              />
+            </div>
+            <div className="message-functionality__file relative">
+              <ImFilesEmpty className="w-6 h-6 text-slate-500 cursor-pointer" />
+              <input
+                type="file"
+                className="w-6 h-6 text-slate-500 absolute top-0 left-0 opacity-0 cursor-pointer"
+              />
             </div>
           </div>
           <input
             className={`w-full outline-none text-lg px-3 py-1 rounded-2xl ${
-              theme === "dark" ? "bg-slate-500" : "bg-slate-200"
+              theme === "dark" ? "bg-slate-700" : "bg-slate-200"
             }`}
             type="text"
             placeholder="Message..."
