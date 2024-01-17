0d5cbd9da7ac4f0edd8d545f60e943c83e85bfa9
pvictordev <pvictor0110@gmail.com>
2024-01-17 06:58:33 +0200

    removed comments

--- src/components/Dialogue.tsx
+++ src/components/Dialogue.tsx
@@ -97,59 +97,6 @@ export default function Dialogue({
       >
         {/* <NoMessage /> */}
         <div className="section-message overflow-y-scroll no-scrollbar">
-          {/* <div
-            className="message-left flex flex-row-reverse my-10 items-center justify-end relative round"
-            onMouseEnter={() => setShowMessageActionsLeft(true)}
-            onMouseLeave={() => setShowMessageActionsLeft(false)}
-          >
-            <div className="message-left__block flex items-center gap-x-3">
-              <div className="message-left__text p-3 max-w-xl ml-8 relative rounded-left bg-white shadow-lg">
-                <span className="text-black">message left</span>
-              </div>
-              <div
-                className={
-                  showMessageActionsLeft
-                    ? "message-left-actions gap-x-3 text-slate-600 flex"
-                    : "hidden"
-                }
-              >
-                <div>
-                  <BsEmojiLaughing className="cursor-pointer" />
-                </div>
-                <div>
-                  <BsReply className="cursor-pointer" />
-                </div>
-              </div>
-            </div>
-          </div> */}
-          {/* <div
-            className="message-right flex flex-row-reverse my-10 items-center justify-start relative round"
-            onMouseEnter={() => setShowMessageActionsRight(true)}
-            onMouseLeave={() => setShowMessageActionsRight(false)}
-          >
-            <div className="message-right__block flex flex-row-reverse items-center gap-x-3">
-              <div className="message-left__text p-3 max-w-xl mr-8 relative rounded-right bg-appColor shadow-lg">
-                <span className="text-white">message right</span>
-              </div>
-              <div
-                className={
-                  showMessageActionsRight
-                    ? "message-left-actions gap-x-3 text-slate-600 flex"
-                    : "hidden"
-                }
-              >
-                <div>
-                  <BsReply className="cursor-pointer" />
-                </div>
-                <div>
-                  <BsEmojiLaughing className="cursor-pointer" />
-                </div>
-                <div>
-                  <FaRegTrashAlt className="cursor-pointer" />
-                </div>
-              </div>
-            </div>
-          </div> */}
           <MessageLeft
             setShowMessageActionsLeft={setShowMessageActionsLeft}
             showMessageActionsLeft={showMessageActionsLeft}
